310eaa4449e7f45057d2c9905a88eb6b60366d2a
unknown <qiangchen.sh@gmail.com>
2025-03-20 16:13:48 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # INPO
+A PyTorch implementation of "[Stable Fair Graph Representation Learning with Lipschitz Constraint](https://openreview.net/pdf?id=oJQWvsStNh)"
+
+## Overview
+**INPO** is a PO-based graph unlearning method to improving the robustness of the model utility to the unlearning process. **The core idea behind INPO is to amplify the influence of unlearned edges and mitigate the tight topological coupling between the forget set and the retain set**, reducing impact on model utility when unlearning specific edges.
+
+## Datasets
+We employ standard and commonly used datasets, which you can download directly according to given links.
+
+| Data       | Link                                                         |
+| ---------- | ------------------------------------------------------------ |
+| Cora       | https://github.com/abojchevski/graph2gauss/raw/master/data/cora.npz |
+| DBLP       | https://github.com/abojchevski/graph2gauss/raw/master/data/dblp.npz |
+| PubMed     | https://github.com/abojchevski/graph2gauss/raw/master/data/pubmed.npz |
+| CS         | https://github.com/shchur/gnn-benchmark/blob/master/data/npz/ms_academic_cs.npz |
+| OGB-Collab | https://ogb.stanford.edu/docs/linkprop/#ogbl-collab          |
+
+## Reproduction
+To reproduce our results, please run:
+```shell
+bash run.sh
+```
+
+## Hyper-parameter Setting
+For easy reproduction, we provide detailed hyper-parameter setting.
+
+![](C:\Users\86198\Desktop\TrustworthyGNN\Code\INPO\figs\settings.png)
--- run.sh
+++ run.sh
@@ -7,20 +7,6 @@ python train_gnn.py --dataset ogbl-collab
 
 # Unlearn Then we can delete information from the trained GNN model. Based on what you want to delete, run one of the three scrips
 # To unlearn edges, please run
-python delete_gnn.py --unlearning_model gnndelete --df_size 0.5
-python delete_gnn.py --dataset Cora --unlearning_model gnndelete --df_size 1.0
-python delete_gnn.py --dataset PubMed --unlearning_model NPO --df_size 0.5 --df out
-python delete_gnn.py --dataset PubMed --unlearning_model gradient_ascent --df_size 0.5 --df out
-python delete_gnn.py --dataset PubMed --unlearning_model gnndelete_all --df_size 0.5 --df out 
-python delete_gnn.py --dataset DBLP --unlearning_model gnndelete --df_size 0.5 --df out
-python delete_gnn.py --dataset ogbl-collab --unlearning_model gnndelete_all --df_size 0.5 --df out 
-python delete_gnn.py --dataset DBLP --unlearning_model gnndelete_all --df_size 0.5 --df out --loss_type both_layerwise
-python delete_gnn.py --dataset DBLP --unlearning_model gradient_ascent --df_size 0.5 --df out
-
-python delete_gnn.py --dataset DBLP --unlearning_model gnndelete_lora --df_size 0.5 --df out --loss_type both_layerwise
-
-
-# overall
 python delete_gnn.py --dataset Cora --unlearning_model retrain --df_size 0.5 --df out
 python delete_gnn.py --dataset Cora --unlearning_model gradient_ascent --df_size 0.5 --df out
 python delete_gnn.py --dataset Cora --unlearning_model gif --df_size 0.5 --df out
@@ -49,33 +35,9 @@ python delete_gnn.py --dataset PubMed --unlearning_model NPO --df_size 0.5 --df 
 python delete_gnn.py --dataset CS --unlearning_model gif --df_size 0.5 --df out
 python delete_gnn.py --dataset CS --unlearning_model utu --df_size 0.5 --df out
 
-
 python delete_gnn.py --dataset ogbl-collab --unlearning_model retrain --df_size 0.5 --df out
 python delete_gnn.py --dataset ogbl-collab --unlearning_model gradient_ascent --df_size 0.5 --df out
 python delete_gnn.py --dataset ogbl-collab --unlearning_model gif --df_size 0.5 --df out
 python delete_gnn.py --dataset ogbl-collab --unlearning_model gnndelete_all --df_size 0.5 --df out
 python delete_gnn.py --dataset ogbl-collab --unlearning_model utu --df_size 0.5 --df out
 python delete_gnn.py --dataset ogbl-collab --unlearning_model NPO --df_size 0.5 --df out
-
-
-python delete_gnn.py --dataset CS --unlearning_model NPO --df_size 0.5 --df out
-python delete_gnn.py --dataset CS --unlearning_model gnndelete_all --df_size 0.5 --df out 
-
-
-
-
-python delete_gnn.py --dataset DBLP --unlearning_model gnndelete_all --df_size 0.5 --df out 
-python delete_gnn.py --dataset DBLP --unlearning_model utu --df_size 0.5 --df out 
-
-python delete_gnn.py --dataset DBLP --unlearning_model gnndelete_NPO --df_size 0.5 --df out --loss_type both_layerwise
-
-
-# To unlearn nodes, please run
-python delete_nodes.py
-
-# To unlearn node features, please run
-python delete_node_feature.py
-
-
-
-python delete_gnn.py --dataset Cora --unlearning_model KTO --df_size 0.5 --df out
